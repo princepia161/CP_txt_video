a5ca26f3486a31469a3cf0d017c5864178d678b8
princepia161 <42060271+princepia161@users.noreply.github.com>
2026-05-05 15:57:30 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "ROWDY"
-BOT_TOKEN = "8066683855:AAF14YAHv8EN3tt7FLq-Dv6z05dlbnQkpgw"
-API_ID = 27679306
-API_HASH = "d0f390f798eec4ddb9246a00a44b7a3f"
-MONGO_URI = "mongodb+srv://rowdybro013:DmnZ3s14aRbfA8tb@cluster01.ramjm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01"
+BOT_NAME = "Genx"
+BOT_TOKEN = "8564398983:AAHuuntq53gVgwIxQash63fatvHq-27BJTc"
+API_ID = 20807000
+API_HASH = "cde2366a7c61e23f4cb44618cbe6cf70"
+MONGO_URI = "mongodb+srv://princepia161_db_user:KZWhQUyEe59dF1jh@cluster0.uhoxe0h.mongodb.net/?appName=Cluster0"
