d64ab999d7f8b690eb1b483aebbfc29aabbb871c
princepia161 <42060271+princepia161@users.noreply.github.com>
2026-05-05 15:53:18 +0530

    Add files via upload

---
+++ modules/config.py
@@ -0,0 +1,6 @@
+# config.py
+BOT_NAME = "ROWDY"
+BOT_TOKEN = "8066683855:AAF14YAHv8EN3tt7FLq-Dv6z05dlbnQkpgw"
+API_ID = 27679306
+API_HASH = "d0f390f798eec4ddb9246a00a44b7a3f"
+MONGO_URI = "mongodb+srv://rowdybro013:DmnZ3s14aRbfA8tb@cluster01.ramjm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01"
