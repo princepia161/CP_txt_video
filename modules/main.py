55825542627c9d9e7ff7f7e113b6a15d38335936
princepia161 <42060271+princepia161@users.noreply.github.com>
2026-05-05 17:57:22 +0530

    Comment out document sending in main.py
    Comment out the document sending line in main.py.

--- modules/main.py
+++ modules/main.py
@@ -719,7 +719,7 @@ async def luminant_command(bot: Client, m: Message):
     input: Message = await bot.listen(editable.chat.id)
     if input.document:
         x = await input.download()        
-        await bot.send_document(log_channel_id, x)                    
+        #await bot.send_document(log_channel_id, x)                    
         await input.delete(True)
         file_name, ext = os.path.splitext(os.path.basename(x))
         credit = my_name
