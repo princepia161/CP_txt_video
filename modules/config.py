b0f181fc3109f79339a920df42486d731d0c0d2f
princepia161 <42060271+princepia161@users.noreply.github.com>
2026-05-05 16:19:55 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -3,4 +3,4 @@ BOT_NAME = "Genx"
 BOT_TOKEN = "8564398983:AAHuuntq53gVgwIxQash63fatvHq-27BJTc"
 API_ID = 20807000
 API_HASH = "cde2366a7c61e23f4cb44618cbe6cf70"
-MONGO_URI = "mongodb+srv://princepia161_db_user:KZWhQUyEe59dF1jh@cluster0.uhoxe0h.mongodb.net/?appName=Cluster0"
+MONGO_URI = "mongodb+srv://princepia161_db_user:KZWhQUyEe59dF1jh@cluster0.uhoxe0h.mongodb.net/?retryWrites=true&w=majority"
